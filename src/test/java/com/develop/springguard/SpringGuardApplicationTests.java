4ee4b5c76dffb906134e94587845da614b535d02
JH1Yoon <wlgus4589@naver.com>
2025-05-12 19:32:34 +0900

    init commit

---
+++ src/test/java/com/develop/springguard/SpringGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.springguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
